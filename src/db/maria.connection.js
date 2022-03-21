3fc10de45f13a66419fe791557d8f20a8dda21e3
York Treinies <york.privat@gmail.com>
2022-03-21 17:54:44 +0100

    db ref

--- src/db/maria.connection.js
+++ src/db/maria.connection.js
@@ -1,9 +1,9 @@
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
-    host: '164.92.133.72',
-    user: 'admin',
-    password: 'safe',
-    database: 'lfnine_tickets',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     charset: 'UTF8MB4'
 });
 
